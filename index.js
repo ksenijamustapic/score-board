1df4426451de5fb23f2c8d9944e803c71a32a176
ksenijamustapic <119668557+ksenijamustapic@users.noreply.github.com>
2022-12-17 19:58:34 +0100

    An initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+
+let countEl = document.getElementById("count-el")
+let count = 0
+
+function add1() {
+    count += 1
+    countEl.textContent = count
+}
+
+function add2() {
+    count += 2
+    countEl.textContent = count
+}
+
+function add3() {
+    count += 3
+    countEl.textContent = count
+}
+
+
+let countGuestEl = document.getElementById("countguest-el")
+let countGuest = 0 
+
+function addOne() {
+    countGuest += 1
+    countGuestEl.textContent = countGuest
+}
+
+function addTwo() {
+    countGuest += 2
+    countGuestEl.textContent = countGuest
+}
+
+function addThree() {
+    countGuest += 3
+    countGuestEl.textContent = countGuest
+}
